558d7655b72c21c475976e0a57caea4df3fe3aae
Alex X Xu <xualexander1@gmail.com>
2018-04-07 15:19:30 -0700

    api config

---
+++ src/config/api.js
@@ -0,0 +1,36 @@
+const api_base = "localhost:3000/api/v1";
+
+const car_base = `${api_base}/cars"`;
+const rating_base = `${api_base}/ratings`;
+const ride_base = `${api_base}/rides`;
+const user_base = `${api_base}/users`;
+
+const API = {
+  car: {
+    list: car_base,
+    show: `${car_base}/`,
+    create: car_base,
+    delete: `${car_base}/`
+  },
+  rating: {
+    list: rating_base,
+    show: `${rating_base}/`,
+    create: rating_base,
+    delete: `${rating_base}/`
+  },
+  ride: {
+    list: ride_base,
+    show: `${ride_base}/`,
+    post: ride_base,
+    update: ride_base,
+    delete: `${ride_base}/`
+  },
+  user: {
+    list: user_base,
+    show: `${user_base}/`,
+    me: `${user_base}/@me`,
+    create: `${api_base}/login`
+  }
+};
+
+export { API };
