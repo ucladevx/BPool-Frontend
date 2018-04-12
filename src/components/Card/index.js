b4bc88d4096f14776659eefbad47b91becc21fbc
Richard Wen <richard.wen@s-169-232-219-10.resnet.ucla.edu>
2018-04-11 22:12:57 -0700

    Fix some dependencies/styling

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,6 @@ import { Card } from "react-materialize";
 const GenericCard = ({ date, price }) => {
   return (
     <Card>
-      {" "}
       {date} <span style={{ float: "right" }}> {price} </span>
     </Card>
   );
