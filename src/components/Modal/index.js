924b26ecf969b0ae55b6887979cfb469907d141b
rishub <rishub@gmail.com>
2018-05-02 14:32:20 -0700

    allow editing of ride

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,27 +3,19 @@ import React from "react";
 import Button from "../Button";
 
 import Modal from "react-responsive-modal";
-import { Row, Col } from "react-materialize";
+import { Row, Col, Input } from "react-materialize";
 
 class RideModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      form: {
-        seats: "",
-        start_city: "",
-        end_city: "",
-        start_dest_lat: "",
-        start_dest_lon: "",
-        end_dest_lat: "",
-        end_dest_lon: "",
-        price_per_seat: "",
-        info: "",
-      },
+      trip: props.trip,
+      form: props.trip,
       editing: false,
     };
     this.closeModal = this.closeModal.bind(this);
     this.delete = this.delete.bind(this);
+    this.cancel = this.cancel.bind(this);
     this.edit = this.edit.bind(this);
     this.save = this.save.bind(this);
   }
@@ -33,7 +25,7 @@ class RideModal extends React.Component {
   }
 
   async delete() {
-    await this.props.delete(this.props.trip.id);
+    await this.props.delete(this.state.trip.id);
     this.props.refresh();
   }
 
@@ -47,29 +39,94 @@ class RideModal extends React.Component {
     //TODO: PUT new ride details
     this.setState({
       editing: false,
+      trip: this.state.form,
+    });
+  }
+
+  cancel() {
+    this.setState({
+      editing: false,
+      form: this.state.trip,
+    });
+  }
+
+  handleFormChange(value) {
+    this.setState({
+      form: {
+        ...this.state.form,
+        ...value,
+      },
     });
   }
 
   render() {
-    const { trip, scrollbox, driver, visible } = this.props;
-    const { form, editing } = this.state;
+    const { scrollbox, driver, visible } = this.props;
+    const { trip, form, editing } = this.state;
+
     return (
-      <Modal open={visible} onClose={this.closeModal}>
+      <Modal onClose={this.closeModal} open={true}>
         <h2>
           {trip.start_city} to {trip.end_city}
         </h2>
-        <h4> ${trip.price_per_seat} per seat </h4>
-        <h4> {trip.seats} seats available </h4>
-        <p> {trip.info} </p>
+        {editing ? (
+          <div>
+            {/* TODO: add icons before input fields */}
+            <Input
+              label="Price per seat"
+              defaultValue={form.price_per_seat}
+              onChange={(e, value) =>
+                this.handleFormChange({ price_per_seat: value })
+              }
+            />
+            <Input
+              label="# Seats Available"
+              defaultValue={form.seats}
+              onChange={(e, value) => this.handleFormChange({ seats: value })}
+            />
+            <Input
+              type="textarea"
+              label="Trip Info"
+              defaultValue={form.info}
+              onChange={(e, value) => this.handleFormChange({ info: value })}
+            />
+          </div>
+        ) : (
+          <div>
+            <h4> ${trip.price_per_seat} per seat </h4>
+            <h4> {trip.seats} seats available </h4>
+            <p> {trip.info} </p>
+          </div>
+        )}
+
         <Row>
-          <Col>
-            {driver && <Button label="Delete" onClick={this.delete} />}
-            {!driver && <Button label="Contact driver" onClick={this.delete} />}
-          </Col>
-          <Col>
-            {!editing && driver && <Button label="Edit" onClick={this.edit} />}
-            {editing && driver && <Button label="Save" onClick={this.save} />}
-          </Col>
+          {!editing ? (
+            <div>
+              <Col>
+                {driver && (
+                  <Button
+                    className="delete"
+                    label="Delete"
+                    onClick={this.delete}
+                  />
+                )}
+                {!driver && (
+                  <Button
+                    className="delete"
+                    label="Contact driver"
+                    onClick={this.delete}
+                  />
+                )}
+              </Col>
+              <Col>{driver && <Button label="Edit" onClick={this.edit} />}</Col>
+            </div>
+          ) : (
+            <div>
+              <Col>
+                {driver && <Button label="Cancel" onClick={this.cancel} />}
+              </Col>
+              <Col>{driver && <Button label="Save" onClick={this.save} />}</Col>
+            </div>
+          )}
         </Row>
       </Modal>
     );
